c696e28dcf0eda71fd6245048e3419c42ec6dd93
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:36:04 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const[name,setName]=useState('');
+  const[greeting, setGreeting]=
   return (
     <div className="App">
       <h2>Hey!!! Greeting</h2>
