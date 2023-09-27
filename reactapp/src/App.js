fda8b6d4aba28037cef316beb384b0fda95ba3e9
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:41:04 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,10 +4,22 @@ import React, {useState} from 'react';
 
 function App() {
   const[name,setName]=useState('');
-  const[greeting, setGreeting]=
+  const[greeting, setGreeting]=useState('');
+  const hadleChange =(event)=>{
+    setName(event.target.value);
+  };
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    if(name.trim()!==''){
+      setGreeting(`Hello,$(name)!`);
+    }
+  };
+
   return (
     <div className="App">
       <h2>Hey!!! Greeting</h2>
+      <form onSubmit={handleSubmit} data.testid='form'>
+      <label data.testid="label">Enter your 
     </div>
   );
 }
