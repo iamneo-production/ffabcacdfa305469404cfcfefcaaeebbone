9f1be85492ec6264c801e9c929183f7c0d14a450
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:46:04 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 
 function App() {
   const[name,setName]=useState('');
   const[greeting, setGreeting]=useState('');
-  const hadleChange =(event)=>{
+  const handleChange =(event)=>{
     setName(event.target.value);
   };
   const handleSubmit=(event)=>{
@@ -18,8 +17,16 @@ function App() {
   return (
     <div className="App">
       <h2>Hey!!! Greeting</h2>
-      <form onSubmit={handleSubmit} data.testid='form'>
-      <label data.testid="label">Enter your 
+      <form onSubmit={handleSubmit} data-testid='form'>
+      <label data-testid="label">Enter your name</label>
+      <input
+      type="text"
+      value={name}
+      onChange={handleChange}
+      /> 
+      <button type="submit" data-testid="buttonElement">Say Hello</button>
+      </form>
+      {greeting && <h2>{greeting}</h2>}
     </div>
   );
 }
